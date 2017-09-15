80e498403f8b06d1d07674530436f97984c27c2f
Hendro Steven <hendro.steven@gmail.com>
2017-09-15 17:38:36 +0800

    logout

--- demo-angular/src/app/app.component.ts
+++ demo-angular/src/app/app.component.ts
@@ -1,17 +1,35 @@
 import {Component} from '@angular/core';
 import {Router} from "@angular/router";
 
-@Component({
-  selector: 'app-root', 
-  styleUrls: ['./app.component.css'], 
-  templateUrl: './app.component.html'})
+@Component({selector: 'app-root', styleUrls: ['./app.component.css'], templateUrl: './app.component.html'})
 export class AppComponent {
 
-  constructor(private router : Router){
-    
-  }
+  constructor(private router : Router) {}
 
   ngOnInit() {
-    this.router.navigate(["list-customer"]);    
+    this
+      .router
+      .navigate(["list-customer"]);
+  }
+
+  isLogin() {
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  onRegister() {
+    // this
+    //   .router
+    //   .navigate(["register"]);
+  }
+
+  logout() {
+    localStorage.clear();
+    this
+      .router
+      .navigate(["login-account"]);
   }
 }
